dffdb8db3d8c4c4dc8e76d84772f53883010bd5a
Brad Lhotsky <brad@divisionbyzero.net>
2013-06-21 18:22:12 +0000

    Import of BLHOTSKY/App-ElasticSearch-Utilities-1.002 from CPAN.
    gitpan-cpan-distribution: App-ElasticSearch-Utilities
    gitpan-cpan-version:      1.002
    gitpan-cpan-path:         BLHOTSKY/App-ElasticSearch-Utilities-1.002.tar.gz
    gitpan-cpan-author:       BLHOTSKY
    gitpan-cpan-maturity:     released

---
+++ lib/App/ElasticSearch/Utilities.pm
@@ -0,0 +1,222 @@
+# ABSTRACT: Utilities for Monitoring ElasticSearch
+package App::ElasticSearch::Utilities;
+
+our $VERSION = '1.002'; # VERSION
+
+use strict;
+use warnings;
+
+use IPC::Run3;
+use Term::ANSIColor;
+use YAML;
+use Getopt::Long qw(:config pass_through);
+use Sub::Exporter -setup => {
+    exports => [
+        qw(output verbose debug debug_var)
+    ],
+};
+
+# Extract the basics from the command line
+my %opt = ();
+GetOptions(\%opt,
+    'color',
+    'csv',
+    'verbose+',
+    'debug',
+    'quiet',
+);
+# Set defaults
+my %DEF = (
+    DEBUG       => $opt{debug} || 0,
+    VERBOSE     => $opt{verbose} || 0,
+    COLOR       => $opt{color} || git_color_check(),
+    KV_FORMART  => $opt{csv} ? ',' : ':',
+    QUIET       => $opt{quiet} || 0,
+);
+debug_var(\%DEF);
+
+sub def { return exists $DEF{$_[0]} ? $DEF{$_[0]} : undef }
+
+sub git_color_check {
+    my @cmd = qw(git config --global --get color.ui);
+    my($out,$err);
+    eval {
+        run3(\@cmd, undef, \$out, \$err);
+    };
+    if( $@  || $err ) {
+        debug("git_color_check error: $err ($@)");
+        return 0;
+    }
+    debug("git_color_check out: $out");
+    if( $out =~ /auto/ || $out =~ /true/ ) {
+        return 1;
+    }
+    return 0;
+}
+sub colorize {
+    my ($color,$string) = @_;
+
+   if( defined $color && $DEF{COLOR} ) {
+        $string=colored([ $color ], $string);
+    }
+    return $string;
+}
+sub output {
+    my $opts = ref $_[0] eq 'HASH' ? shift @_ : {};
+
+    # Quiet mode!
+    return if $DEF{quiet};
+
+    # Input/output Arrays
+    my @input = @_;
+    my @output = ();
+
+    # Remove line endings
+    chomp(@input);
+
+    # Determine the color
+    my $color = exists $opts->{color} && defined $opts->{color} ? $opts->{color} : undef;
+
+    # Determine indentation
+    my $indent = exists $opts->{indent} ? " "x(2*$opts->{indent}) : '';
+
+    # Determine if we're doing Key Value Pairs
+    my $DO_KV = (scalar(@input) % 2 == 0 ) && (exists $opts->{kv} && $opts->{kv} == 1) ? 1 : 0;
+
+    if( $DO_KV ) {
+        while( @input ) {
+            my $k = shift @input;
+            # We only colorize the value
+            my $v = colorize($color, shift @input );
+            push @output, join($DEF{KV_FORMAT}, $k, $v);
+        }
+    }
+    else {
+        foreach my $msg ( map { colorize($color, $_); } @input) {
+            push @output, $msg;
+        }
+    }
+    my $out_handle = exists $opts->{stderr} && $opts->{stderr} ? \*STDERR : \*STDOUT;
+    # Do clearing
+    print $out_handle "\n"x$opts->{clear} if exists $opts->{clear};
+    # Print output
+    print $out_handle "${indent}$_\n" for @output;
+}
+sub verbose {
+    my $opts = ref $_[0] eq 'HASH' ? shift @_ : {};
+    $opts->{level} = 1 unless exists $opts->{level};
+    my @msgs=@_;
+
+    if( !$DEF{DEBUG} ) {
+        return unless $DEF{VERBOSE} >= $opts->{level};
+    }
+    output( $opts, @msgs );
+}
+sub debug {
+    my $opts = ref $_[0] eq 'HASH' ? shift @_ : {};
+    my @msgs=@_;
+    return unless $DEF{DEBUG};
+    output( $opts, @msgs );
+}
+sub debug_var {
+    return unless $DEF{DEBUG};
+
+    my $opts = {clear => 1};
+    if( ref $_[0] eq 'HASH' && defined $_[1] && ref $_[1] ) {
+        my $ref = shift;
+        foreach my $k (keys %{ $ref } ) {
+            $opts->{$k} = $ref->{$k};
+        };
+    }
+    output( $opts, Dump shift);
+}
+
+
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+App::ElasticSearch::Utilities - Utilities for Monitoring ElasticSearch
+
+=head1 VERSION
+
+version 1.002
+
+=head1 SYNOPSIS
+
+This library contains utilities for unified interfaces in the scripts.
+
+This a set of utilities to make monitoring ElasticSearch clusters much simpler.
+
+Included is:
+
+    scripts/es-status.pl - Command line utility for ES Metrics
+    scripts/es-metrics-to-graphite.pl - Send ES Metrics to Graphite or Cacti
+    scripts/es-nagios-check.pl - Monitor ES remotely or via NRPE with this script
+    scripts/es-daily-index-maintenance.pl - Perform index maintenance on daily indexes
+    scripts/es-copy-index.pl - Copy an index from one cluster to another
+
+The App::ElasticSearch::Utilities module simply serves as a wrapper around the scripts for packaging and
+distribution.
+
+=head1 INSTALL
+
+Recommended install with L<CPAN Minus|http://cpanmin.us>:
+
+    cpanm App::ElsticSearch::Utilities
+
+You can also use CPAN:
+
+    cpan App::ElsticSearch::Utilities
+
+Or if you'd prefer to manually install:
+
+    export RELEASE=<CurrentRelease>
+
+    wget --no-check-certificate https://github.com/reyjrar/es-utils/blob/master/releases/App-ElasticSearch-Utilities-$RELEASE.tar.gz?raw=true -O es-utils.tgz
+
+    tar -zxvf es-utils.tgz
+
+    cd App-ElasticSearch-Utilities-$RELEASE
+
+    perl Makefile.PL
+
+    make
+
+    make install
+
+This will take care of ensuring all the dependencies are satisfied and will install the scripts into the same
+directory as your Perl executable.
+
+=head2 USAGE
+
+The tools are all wrapped in their own documentation, please see:
+
+    es-status.pl --help
+    es-metric-to-graphite.pl --help
+    es-nagios-check.pl --help
+    es-daily-index-maintenance.pl --help
+    es-copy-index.pl --help
+
+For individual options and capabilities
+
+=head1 AUTHOR
+
+Brad Lhotsky <brad@divisionbyzero.net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2012 by Brad Lhotsky.
+
+This is free software, licensed under:
+
+  The (three-clause) BSD License
+
+=cut
---
+++ scripts/es-daily-index-maintenance.pl
@@ -0,0 +1,244 @@
+#!/usr/bin/env perl
+# PODNAME: es-daily-index-maintenance.pl
+# ABSTRACT: Run to prune old indexes and optimize existing
+use strict;
+use warnings;
+
+BEGIN {
+    # Clear out any proxy settings
+    delete $ENV{$_} for qw(http_proxy HTTP_PROXY);
+}
+
+use DateTime;
+use ElasticSearch;
+use JSON::XS;
+use LWP::Simple;
+use Getopt::Long;
+use Pod::Usage;
+use App::ElasticSearch::Utilities qw(:all);
+
+#------------------------------------------------------------------------#
+# Argument Collection
+my %opt;
+GetOptions(\%opt,
+    'local',
+    'all',
+    'host:s',
+    'port:i',
+    'delete',
+    'delete-days:i',
+    'optimize',
+    'optimize-days:i',
+    'index-basename:s',
+    'date-separator:s',
+    'timezone:s',
+    # Basic options
+    'help|h',
+    'manual|m',
+);
+
+#------------------------------------------------------------------------#
+# Documentations!
+pod2usage(1) if $opt{help};
+pod2usage(-exitstatus => 0, -verbose => 2) if $opt{manual};
+
+#------------------------------------------------------------------------#
+# Host or Local
+pod2usage(1) unless defined $opt{local} or defined $opt{host};
+
+my %CFG = (
+    'optimize-days'  => 1,
+    'delete-days'    => 90,
+    'index-basename' => 'logstash',
+    'date-separator' => '.',
+    timezone         => 'Europe/Amsterdam',
+    port             => 9200,
+    delete           => 0,
+    optimize         => 0,
+);
+# Extract from our options if we've overridden defaults
+foreach my $setting (keys %CFG) {
+    $CFG{$setting} = $opt{$setting} if exists $opt{$setting} and defined $opt{$setting};
+}
+$CFG{delete} = $CFG{optimize} = 1 if exists $opt{all} and $opt{all};
+if ( $CFG{delete} == 0 and $CFG{optimize} == 0 ) {
+    pod2usage(1);
+}
+
+# Create the target uri for the ES Cluster
+my $TARGET = exists $opt{host} && defined $opt{host} ? $opt{host} : 'localhost';
+$TARGET .= ":$CFG{port}";
+debug("Target is: $TARGET");
+debug_var(\%CFG);
+
+my $es = ElasticSearch->new(
+    servers   => [ $TARGET ],
+    transport => 'http',
+    timeout   => 0,     # Do Not Timeout
+);
+
+# Delete Indexes older than a certain point
+my $DEL      = DateTime->now(time_zone => $CFG{timezone})->truncate( to => 'day' )->subtract( days => $CFG{'delete-days'} );
+my $OPTIMIZE = DateTime->now(time_zone => $CFG{timezone})->truncate( to => 'day' )->subtract( days => $CFG{'optimize-days'} );
+my $d_res    = $es->index_stats(
+    index => undef,
+    clear => 1
+);
+# Handle Old and New versions of ElasticSearch
+my $indices = (exists $d_res->{_all}{indices} && ref $d_res->{_all}{indices} eq 'HASH') ? $d_res->{_all}{indices} :
+              (exists $d_res->{indices}       && ref $d_res->{indices} eq 'HASH')       ? $d_res->{indices} :
+              undef;
+
+if ( !defined $indices ) {
+    output({color=>"red"}, "Unable to locate indices in status!");
+    exit 1;
+}
+# Loop through the indices and take appropriate actions;
+foreach my $index (sort keys %{ $indices }) {
+    verbose("$index being evaluated");
+
+
+    my ($basename,$dateStr) = split /\-/, $index;
+    debug("Basename: $basename");
+    debug("date string: $dateStr");
+    next unless $basename eq $CFG{'index-basename'};
+
+    my $sep = $CFG{'date-separator'};
+    my @parts = split /\Q$sep\E/, $dateStr;
+    my $idx_dt = DateTime->new( year => $parts[0], month => $parts[1], day => $parts[2] );
+
+    # Delete the Index if it's too old
+    if( $CFG{delete} && $idx_dt < $DEL ) {
+        output({color=>"red"}, "$index will be deleted.");
+        eval {
+            my $rc = $es->delete_index( index => $index );
+        };
+        next;
+    }
+
+    # Run optimize?
+    if( $CFG{optimize} ) {
+        my $segment_ratio = undef;
+        eval {
+            my $json = get( qq{http://$TARGET/$index/_segments} );
+            my $res = decode_json( $json );
+            my $shard_data = $res->{indices}{$index}{shards};
+            my $shards = 0;
+            my $segments = 0;
+            foreach my $id (keys %{$shard_data} ){
+                $segments += $shard_data->{$id}[0]{num_search_segments};
+                $shards++;
+            }
+            if( $shards > 0 ) {
+                $segment_ratio = sprintf( "%0.2f", $segments / $shards );
+            }
+        };
+
+        if( $idx_dt <= $OPTIMIZE && defined($segment_ratio) && $segment_ratio > 1 ) {
+            my $error = undef;
+            verbose({color=>"yellow"}, "$index: required (segment_ratio: $segment_ratio).");
+
+            eval {
+                my $o_res = $es->optimize_index(
+                    index            => $index,
+                    max_num_segments => 1,
+                    wait_for_merge   => 0,
+                );
+                output({color=>"green"}, "$index: $o_res->{_shards}{successful} of $o_res->{_shards}{total} shards optimized.");
+            };
+            if( my $error = $@ ) {
+                output({color=>"red"}, "$index: Encountered error during optimize: $error");
+            }
+        }
+        else {
+            if( $segment_ratio > 1 ) {
+                verbose("$index is active not optimizing (segment_ratio:$segment_ratio)");
+            }
+            else {
+                verbose("$index already optimized");
+            }
+        }
+    }
+}
+
+__END__
+
+=pod
+
+=head1 NAME
+
+es-daily-index-maintenance.pl - Run to prune old indexes and optimize existing
+
+=head1 VERSION
+
+version 1.002
+
+=head1 SYNOPSIS
+
+es-daily-index-maintenance.pl --all --local
+
+Options:
+
+    --help              print help
+    --manual            print full manual
+    --local             Poll localhost and use name reported by ES
+    --host|-H           Host to poll for statistics
+    --local             Assume localhost as the host
+    --all               Run delete and optimize
+    --delete            Run delete indexes older than
+    --delete-days       Age of oldest index to keep (default: 90)
+    --optimize          Run optimize on indexes
+    --optimize-days     Age of first index to optimize (default: 1)
+    --index-basename    Default is 'logstash'
+    --date-separator    Default is '.'
+    --quiet             Ideal for running on cron, only outputs errors
+    --verbose           Send additional messages to STDERR
+
+=head1 DESCRIPTION
+
+This script assists in maintaining the indexes for logging clusters through
+routine deletion and optimization of indexes.
+
+Use with cron:
+
+    22 4 * * * es-daily-index-maintenance.pl --local --all --delete-days=180
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<help>
+
+Print this message and exit
+
+=item B<manual>
+
+Print this message and exit
+
+=item B<local>
+
+Optional, operate on localhost (if not specified, --host required)
+
+=item B<host>
+
+Optional, the host to maintain (if not specified --local required)
+
+=item B<verbose>
+
+Verbose stats, to not interfere with cacti, output goes to STDERR
+
+=back
+
+=head1 AUTHOR
+
+Brad Lhotsky <brad@divisionbyzero.net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2012 by Brad Lhotsky.
+
+This is free software, licensed under:
+
+  The (three-clause) BSD License
+
+=cut
